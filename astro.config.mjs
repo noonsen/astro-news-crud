ce0f9929fb6a4325cc867221f7ae118e726ced58
Bonchol <alonzoluis930@gmail.com>
2024-03-01 18:53:24 +0800

    fix: import netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from '@astrojs/cloudflare';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: true,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: true
+  })],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: netlify()
 });
